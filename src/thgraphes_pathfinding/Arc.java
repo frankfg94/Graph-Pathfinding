45748887624ae3d7856f452b5baf9a128a4a8159
François GILLIOEN <francois.gillioen@efrei.net>
2019-03-11 21:22:10 +0100

    2ème Constructeur avec des objets sommets

--- src/thgraphes_pathfinding/Arc.java
+++ src/thgraphes_pathfinding/Arc.java
@@ -11,14 +11,19 @@ package thgraphes_pathfinding;
  */
 public class Arc 
 {
-    public Vertex initialExtremity;
-    public Vertex terminalExtremity;
+    public Vertex initialExtremity; // pas utilisé pour l'instant à par dans le constructeur Arc(int value, Vertex initExtremity, Vertex termExtremity)
+    public Vertex terminalExtremity; // pas utilisé pour l'instant à par dans le constructeur Arc(int value, Vertex initExtremity, Vertex termExtremity)
     public int initExtremityValue = -1;
     public int termExtremityValue = -1;
     public int value = -1;
     
     public Arc(int value, Vertex initExtremity, Vertex termExtremity)
     {
+        this.value=value;
+        this.initialExtremity = initExtremity;
+        this.terminalExtremity = termExtremity;
+        this.initExtremityValue = initExtremity.value;
+        this.termExtremityValue = termExtremity.value;
     }
     
     public Arc(int value, int initExtremityValue, int termExtremityValue)
